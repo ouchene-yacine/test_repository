0c35d39a922d4e50421fa7849f080c3be27b2ac6
ouchene-yacine <ouchene-yacine@email.com>
2018-02-14 12:58:39 +0100

    change the loop

--- GitProject/src/Main.java
+++ GitProject/src/Main.java
@@ -9,9 +9,9 @@ public class Main {
 	public static void main(String[] args) {
 		
 		String [] names= {"name1","name2","name3","name4"};
-		
-		for (String name : names) {
-			System.out.println("Name: "+name);
+		//add comments
+		for (int i=0; i<names.length;i++) {
+			System.out.println("Name: "+names[i]);
 		}
 		
 	}
