805c8a874160825512b126f601399e2cef7006bc
ouchene-yacine 1 <ouchene-yacine@email.com>
2018-02-14 13:00:09 +0100

    add names, add syso

--- GitProject/src/Main.java
+++ GitProject/src/Main.java
@@ -8,9 +8,10 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		String [] names= {"name1","name2","name3","name4"};
+		String [] names= {"name1","name2","name3","name4","name5","name6"};
 		
 		for (String name : names) {
+			System.out.println("Hello");
 			System.out.println("Name: "+name);
 		}
 		
