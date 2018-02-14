296223db4b9136522f0c28c7352f39bc37d1ae7a
ouchene-yacine <ouchene-yacine@email.com>
2018-02-14 12:41:01 +0100

    commit it Hello git project

---
+++ GitProject/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	
+	public Main() {
+		
+	}
+	
+	public static void main(String[] args) {
+		//comment
+		
+		//comment change again
+		System.out.println("Hello world");
+	}
+}
